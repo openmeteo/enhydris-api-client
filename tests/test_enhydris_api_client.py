2e5f5269a0649045484ef7f3a48fcd4d8ab1c20d
Antonis Christofides <antonis@antonischristofides.com>
2018-10-25 11:14:52 +0300

    Slightly different unit tests

--- tests/test_enhydris_api_client.py
+++ tests/test_enhydris_api_client.py
@@ -9,22 +9,32 @@ import requests
 import enhydris_api_client
 
 
-class SimpleLoginTestCase(TestCase):
-    @mock.patch(
-        "requests.get", **{"return_value.cookies": {"csrftoken": "reallysecret"}}
-    )
-    @mock.patch(
-        "requests.post", **{"return_value.cookies": {"acookie": "a cookie value"}}
-    )
-    def setUp(self, mock_requests_post, mock_requests_get):
-        self.mock_requests_get = mock_requests_get
-        self.mock_requests_post = mock_requests_post
-        self.response_cookies = enhydris_api_client.login(
-            "https://mydomain.com", "admin", "topsecret"
-        )
+def rpatch(*args, **kwargs):
+    """Same as mock.patch with raise_for_status feature.
 
-    def test_makes_post_request(self):
-        self.mock_requests_post.assert_called_once_with(
+    When mocking requests.get or requests.post or similar, instead of
+        @mock.patch("requests.get", ...)
+    use
+        @rpatch("requests.get", ...)
+
+    It's almost the same thing. There are two differences:
+        - If "return_value.status_code" is unspecified, it is set to 200.
+        - If "return_value.status_code" is not between 200 and 400, then
+          return_value.raise_for_status() will raise HTTPError. This will only work if
+          "return_value.status_code" is specified in kwargs.
+    """
+    c = kwargs.setdefault("return_value.status_code", 200)
+    if c < 200 or c >= 400:
+        kwargs["return_value.raise_for_status.side_effect"] = requests.HTTPError
+    return mock.patch(*args, **kwargs)
+
+
+class SimpleLoginTestCase(TestCase):
+    @rpatch("requests.get", **{"return_value.cookies": {"csrftoken": "reallysecret"}})
+    @rpatch("requests.post")
+    def test_makes_post_request(self, mock_requests_post, mock_requests_get):
+        enhydris_api_client.login("https://mydomain.com", "admin", "topsecret")
+        mock_requests_post.assert_called_once_with(
             "https://mydomain.com/accounts/login/",
             headers={
                 "X-CSRFToken": "reallysecret",
@@ -36,19 +46,28 @@ class SimpleLoginTestCase(TestCase):
             allow_redirects=False,
         )
 
-    def test_checks_response_code_for_get(self):
-        self.mock_requests_get.return_value.raise_for_status.assert_called_once_with()
+    @rpatch("requests.get", **{"return_value.status_code": 404})
+    @rpatch("requests.post")
+    def test_checks_response_code_for_get(self, mock_requests_post, mock_requests_get):
+        with self.assertRaises(requests.HTTPError):
+            enhydris_api_client.login("https://mydomain.com", "admin", "topsecret")
 
-    def test_checks_response_code_for_post(self):
-        self.mock_requests_post.return_value.raise_for_status.assert_called_once_with()
+    @rpatch("requests.get", **{"return_value.cookies": {"csrftoken": "reallysecret"}})
+    @rpatch("requests.post", **{"return_value.status_code": 404})
+    def test_checks_response_code_for_post(self, mock_requests_post, mock_requests_get):
+        with self.assertRaises(requests.HTTPError):
+            enhydris_api_client.login("https://mydomain.com", "admin", "topsecret")
 
-    def test_returns_cookies(self):
-        self.assertEqual(self.response_cookies, {"acookie": "a cookie value"})
+    @rpatch("requests.get", **{"return_value.cookies": {"csrftoken": "reallysecret"}})
+    @rpatch("requests.post", **{"return_value.cookies": {"acookie": "a cookie value"}})
+    def test_returns_cookies(self, mock_requests_post, mock_requests_get):
+        c = enhydris_api_client.login("https://mydomain.com", "admin", "topsecret")
+        self.assertEqual(c, {"acookie": "a cookie value"})
 
 
 class LoginWithEmptyUsernameTestCase(TestCase):
-    @mock.patch("requests.get")
-    @mock.patch("requests.post")
+    @rpatch("requests.get")
+    @rpatch("requests.post")
     def setUp(self, mock_requests_post, mock_requests_get):
         self.mock_requests_get = mock_requests_get
         self.mock_requests_post = mock_requests_post
@@ -67,41 +86,39 @@ class LoginWithEmptyUsernameTestCase(TestCase):
 
 
 class GetModelTestCase(TestCase):
-    @mock.patch(
-        "requests.get", **{"return_value.json.return_value": {"hello": "world"}}
-    )
-    def setUp(self, mock_requests_get):
-        self.mock_requests_get = mock_requests_get
-        self.response_data = enhydris_api_client.get_model(
+    @rpatch("requests.get")
+    def test_makes_request(self, mock_requests_get):
+        enhydris_api_client.get_model(
             "https://mydomain.com", {"acookie": "a cookie value"}, "Station", 42
         )
-
-    def test_makes_request(self):
-        self.mock_requests_get.assert_called_once_with(
+        mock_requests_get.assert_called_once_with(
             "https://mydomain.com/api/Station/42/",
             cookies={"acookie": "a cookie value"},
         )
 
-    def test_checks_response_code(self):
-        self.mock_requests_get.return_value.raise_for_status.assert_called_once_with()
+    @rpatch("requests.get", **{"return_value.status_code": 404})
+    def test_checks_response_code(self, mock_requests_get):
+        with self.assertRaises(requests.HTTPError):
+            enhydris_api_client.get_model(
+                "https://mydomain.com", {"acookie": "a cookie value"}, "Station", 42
+            )
 
-    def test_returns_data(self):
-        self.assertEqual(self.response_data, {"hello": "world"})
+    @rpatch("requests.get", **{"return_value.json.return_value": {"hello": "world"}})
+    def test_returns_data(self, mock_requests_get):
+        data = enhydris_api_client.get_model("https://mydomain.com", {}, "Station", 42)
+        self.assertEqual(data, {"hello": "world"})
 
 
 class PostModelTestCase(TestCase):
-    @mock.patch("requests.post", **{"return_value.json.return_value": {"id": 42}})
-    def setUp(self, mock_requests_post):
-        self.mock_requests_post = mock_requests_post
-        self.response_data = enhydris_api_client.post_model(
+    @rpatch("requests.post", **{"return_value.json.return_value": {"id": 42}})
+    def test_makes_request(self, mock_requests_post):
+        enhydris_api_client.post_model(
             "https://mydomain.com",
             {"csrftoken": "reallysecret"},
             "Station",
             data={"location": "Syria"},
         )
-
-    def test_makes_request(self):
-        self.mock_requests_post.assert_called_once_with(
+        mock_requests_post.assert_called_once_with(
             "https://mydomain.com/api/Station/",
             headers={
                 "X-CSRFToken": "reallysecret",
@@ -111,15 +128,29 @@ class PostModelTestCase(TestCase):
             data={"location": "Syria"},
         )
 
-    def test_checks_response_code(self):
-        self.mock_requests_post.return_value.raise_for_status.assert_called_once_with()
+    @rpatch("requests.post", **{"return_value.status_code": 404})
+    def test_checks_response_code(self, mock_requests_post):
+        with self.assertRaises(requests.HTTPError):
+            enhydris_api_client.post_model(
+                "https://mydomain.com",
+                {"csrftoken": "reallysecret"},
+                "Station",
+                data={"location": "Syria"},
+            )
 
-    def test_returns_id(self):
-        self.assertEqual(self.response_data, 42)
+    @rpatch("requests.post", **{"return_value.json.return_value": {"id": 42}})
+    def test_returns_id(self, mock_requests_post):
+        r = enhydris_api_client.post_model(
+            "https://mydomain.com",
+            {"csrftoken": "reallysecret"},
+            "Station",
+            data={"location": "Syria"},
+        )
+        self.assertEqual(r, 42)
 
 
 class DeleteModelTestCase(TestCase):
-    @mock.patch("requests.delete", **{"return_value.status_code": 204})
+    @rpatch("requests.delete", **{"return_value.status_code": 204})
     def test_makes_request(self, mock_requests_delete):
         enhydris_api_client.delete_model(
             "https://mydomain.com", {"csrftoken": "reallysecret"}, "Station", 42
@@ -130,9 +161,9 @@ class DeleteModelTestCase(TestCase):
             cookies={"csrftoken": "reallysecret"},
         )
 
-    @mock.patch("requests.delete", **{"return_value.status_code": 404})
+    @rpatch("requests.delete", **{"return_value.status_code": 404})
     def test_raises_exception_on_error(self, mock_requests_delete):
-        with self.assertRaises(requests.exceptions.HTTPError):
+        with self.assertRaises(requests.HTTPError):
             enhydris_api_client.delete_model(
                 "https://mydomain.com", {"csrftoken": "reallysecret"}, "Station", 42
             )
@@ -155,40 +186,42 @@ test_timeseries_csv_bottom = test_timeseries_csv.splitlines(keepends=True)[-1][0
 
 
 class ReadTsDataTestCase(TestCase):
-    @mock.patch("requests.get", **{"return_value.text": test_timeseries_csv})
-    def setUp(self, mock_requests_get):
-        self.mock_requests_get = mock_requests_get
-        self.response_data = enhydris_api_client.read_tsdata(
+    @rpatch("requests.get", **{"return_value.text": test_timeseries_csv})
+    def test_makes_request(self, mock_requests_get):
+        enhydris_api_client.read_tsdata(
             "https://mydomain.com", {"csrftoken": "reallysecret"}, 42
         )
-
-    def test_makes_request(self):
-        self.mock_requests_get.assert_called_once_with(
+        mock_requests_get.assert_called_once_with(
             "https://mydomain.com/api/tsdata/42/", cookies={"csrftoken": "reallysecret"}
         )
 
-    def test_checks_response_code(self):
-        self.mock_requests_get.return_value.raise_for_status.assert_called_once_with()
+    @rpatch("requests.get", **{"return_value.status_code": 404})
+    def test_checks_response_code(self, mock_requests_get):
+        with self.assertRaises(requests.HTTPError):
+            enhydris_api_client.read_tsdata(
+                "https://mydomain.com", {"csrftoken": "reallysecret"}, 42
+            )
 
-    def test_returns_data(self):
-        pd.testing.assert_frame_equal(self.response_data, test_timeseries_pd)
+    @rpatch("requests.get", **{"return_value.text": test_timeseries_csv})
+    def test_returns_data(self, mock_requests_get):
+        data = enhydris_api_client.read_tsdata(
+            "https://mydomain.com", {"csrftoken": "reallysecret"}, 42
+        )
+        pd.testing.assert_frame_equal(data, test_timeseries_pd)
 
 
 class PostTsDataTestCase(TestCase):
-    @mock.patch("requests.post")
-    def setUp(self, mock_requests_post):
-        self.mock_requests_post = mock_requests_post
+    @rpatch("requests.post")
+    def test_makes_request(self, mock_requests_post):
         enhydris_api_client.post_tsdata(
             "https://mydomain.com",
             {"csrftoken": "reallysecret"},
             42,
             test_timeseries_pd,
         )
-
-    def test_makes_request(self):
         f = StringIO()
         test_timeseries_pd.to_csv(f, header=False)
-        self.mock_requests_post.assert_called_once_with(
+        mock_requests_post.assert_called_once_with(
             "https://mydomain.com/api/tsdata/42/",
             headers={
                 "Content-Type": "application/x-www-form-urlencoded",
@@ -198,38 +231,47 @@ class PostTsDataTestCase(TestCase):
             data={"timeseries_records": f.getvalue()},
         )
 
-    def test_checks_response_code(self):
-        self.mock_requests_post.return_value.raise_for_status.assert_called_once_with()
+    @rpatch("requests.post", **{"return_value.status_code": 404})
+    def test_checks_response_code(self, mock_requests_post):
+        with self.assertRaises(requests.HTTPError):
+            enhydris_api_client.post_tsdata(
+                "https://mydomain.com",
+                {"csrftoken": "reallysecret"},
+                42,
+                test_timeseries_pd,
+            )
 
 
 class GetTsEndDateTestCase(TestCase):
-    @mock.patch("requests.get", **{"return_value.text": test_timeseries_csv})
-    def setUp(self, mock_requests_get):
-        self.mock_requests_get = mock_requests_get
-        self.response_date = enhydris_api_client.get_ts_end_date(
+    @rpatch("requests.get", **{"return_value.text": test_timeseries_csv})
+    def test_makes_request(self, mock_requests_get):
+        enhydris_api_client.get_ts_end_date(
             "https://mydomain.com", {"csrftoken": "reallysecret"}, 42
         )
-
-    def test_makes_request(self):
-        self.mock_requests_get.assert_called_once_with(
+        mock_requests_get.assert_called_once_with(
             "https://mydomain.com/timeseries/d/42/bottom/",
             cookies={"csrftoken": "reallysecret"},
         )
 
-    def test_checks_response_code(self):
-        self.mock_requests_get.return_value.raise_for_status.assert_called_once_with()
+    @rpatch("requests.get", **{"return_value.status_code": 404})
+    def test_checks_response_code(self, mock_requests_get):
+        with self.assertRaises(requests.HTTPError):
+            enhydris_api_client.get_ts_end_date(
+                "https://mydomain.com", {"csrftoken": "reallysecret"}, 42
+            )
 
-    def test_returns_date(self):
-        self.assertEqual(self.response_date, datetime(2014, 1, 5, 8, 0))
+    @rpatch("requests.get", **{"return_value.text": test_timeseries_csv})
+    def test_returns_date(self, mock_requests_get):
+        date = enhydris_api_client.get_ts_end_date(
+            "https://mydomain.com", {"csrftoken": "reallysecret"}, 42
+        )
+        self.assertEqual(date, datetime(2014, 1, 5, 8, 0))
 
 
 class GetTsEndDateEmptyTestCase(TestCase):
-    @mock.patch("requests.get", **{"return_value.text": ""})
-    def setUp(self, mock_requests_get):
-        self.mock_requests_get = mock_requests_get
-        self.response_date = enhydris_api_client.get_ts_end_date(
+    @rpatch("requests.get", **{"return_value.text": ""})
+    def test_returns_date(self, mock_requests_get):
+        date = enhydris_api_client.get_ts_end_date(
             "https://mydomain.com", {"csrftoken": "reallysecret"}, 42
         )
-
-    def test_returns_date(self):
-        self.assertEqual(self.response_date, datetime(1, 1, 1))
+        self.assertEqual(date, datetime(1, 1, 1))
