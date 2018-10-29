cb1eb35e9fcaebd5b8a8fe53bcca80e5942287e0
Antonis Christofides <antonis@antonischristofides.com>
2018-10-29 19:39:44 +0200

    Use requests.Session() instead of manually handling cookies

--- enhydris_api_client/__init__.py
+++ enhydris_api_client/__init__.py
@@ -7,75 +7,64 @@ import pandas as pd
 import requests
 
 
-# My understanding from requests' documentation is that when I make a post
-# request, it shouldn't be necessary to specify Content-Type:
-# application/x-www-form-urlencoded, and that requests adds the header
-# automatically. However, when running in Python 3, apparently requests does
-# not add the header (although it does convert the post data to
-# x-www-form-urlencoded format). This is why the header has been explicitly
-# specified in all post requests.
-
-
 class EnhydrisApiClient:
     def __init__(self, base_url):
         self.base_url = base_url
-        self.cookies = {}
+        self.session = requests.Session()
 
     def login(self, username, password):
-        self.cookies = {}
         if not username:
             return
+
+        # Get a csrftoken
         login_url = urljoin(self.base_url, "accounts/login/")
-        r = requests.get(login_url)
+        r = self.session.get(login_url)
         r.raise_for_status()
-        r1 = requests.post(
-            login_url,
-            headers={
+        self.session.headers.update(
+            {
                 "X-CSRFToken": r.cookies.get("csrftoken", "unspecified CSRF token"),
-                "Referer": login_url,
-                "Content-Type": "application/x-www-form-urlencoded"
-                # See comment about x-www-form-urlencoded above
-            },
-            data="username={}&password={}".format(username, password),
-            cookies=r.cookies,
-            allow_redirects=False,
+
+                # My understanding from requests' documentation is that when I make a
+                # post request, it shouldn't be necessary to specify Content-Type:
+                # application/x-www-form-urlencoded, and that requests adds the header
+                # automatically. However, when running in Python 3, apparently requests
+                # does not add the header (although it does convert the post data to
+                # x-www-form-urlencoded format). This is why I'm specifying it
+                # explicitly.
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
         )
+
+        # Now login
+        data = "username={}&password={}".format(username, password)
+        r1 = self.session.post(login_url, data=data, allow_redirects=False)
         r1.raise_for_status()
-        self.cookies = r1.cookies
+        self.session.headers["X-CSRFToken"] = r1.cookies.get(
+            "csrftoken", "unspecified CSRF token"
+        )
 
     def get_model(self, model, obj_id):
         url = urljoin(self.base_url, "api/{}/{}/".format(model, obj_id))
-        r = requests.get(url, cookies=self.cookies)
+        r = self.session.get(url)
         r.raise_for_status()
         return r.json()
 
     def post_model(self, model, data):
-        r = requests.post(
-            urljoin(self.base_url, "api/{}/".format(model)),
-            headers={
-                "X-CSRFToken": self.cookies.get("csrftoken", "unspecified CSRF token"),
-                "Content-Type": "application/x-www-form-urlencoded"
-                # See comment about x-www-form-urlencoded above
-            },
-            cookies=self.cookies,
-            data=data,
+        r = self.session.post(
+            urljoin(self.base_url, "api/{}/".format(model)), data=data
         )
         r.raise_for_status()
         return r.json()["id"]
 
     def delete_model(self, model, id):
         url = urljoin(self.base_url, "api/{}/{}/".format(model, id))
-        r = requests.delete(
-            url,
-            cookies=self.cookies,
-            headers={"X-CSRFToken": self.cookies["csrftoken"]},
-        )
+        r = self.session.delete(url)
         if r.status_code != 204:
             raise requests.HTTPError()
 
     def read_tsdata(self, ts_id):
         url = urljoin(self.base_url, "api/tsdata/{0}/".format(ts_id))
-        r = requests.get(url, cookies=self.cookies)
+        r = self.session.get(url)
         r.raise_for_status()
         return pd.read_csv(StringIO(r.text), header=None, parse_dates=True, index_col=0)
 
@@ -83,21 +72,13 @@ class EnhydrisApiClient:
         f = StringIO()
         ts.to_csv(f, header=False)
         url = urljoin(self.base_url, "api/tsdata/{}/".format(timeseries_id))
-        r = requests.post(
-            url,
-            data={"timeseries_records": f.getvalue()},
-            headers={
-                "Content-Type": "application/x-www-form-urlencoded",
-                "X-CSRFToken": self.cookies.get("csrftoken"),
-            },
-            cookies=self.cookies,
-        )
+        r = self.session.post(url, data={"timeseries_records": f.getvalue()})
         r.raise_for_status()
         return r.text
 
     def get_ts_end_date(self, ts_id):
         url = urljoin(self.base_url, "timeseries/d/{}/bottom/".format(ts_id))
-        r = requests.get(url, cookies=self.cookies)
+        r = self.session.get(url)
         r.raise_for_status()
         lines = r.text.splitlines()
         lines.reverse()
